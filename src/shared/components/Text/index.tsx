5ac13c4fe0f37f4826a5727fc1292d775be44c60
Ana Ribeiro <lidialira@alu.ufc.br>
2021-10-12 13:00:24 -0300

    creates text component & accessibility font size redux

---
+++ src/shared/components/Text/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+import {ApplicationState} from '~/shared/store';
+
+import * as S from './styles';
+
+interface NewTextProps {
+  fontSize?: number;
+}
+
+const NewText: React.FC<NewTextProps> = ({
+  fontSize = 16,
+  children,
+  ...rest
+}) => {
+  const {delta} = useSelector((state: ApplicationState) => state.font);
+
+  return (
+    <S.Text {...rest} customFontSize={fontSize + delta}>
+      {children}
+    </S.Text>
+  );
+};
+
+export default NewText;
---
+++ src/shared/store/ducks/index.ts
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+
+import font from './font';
+import theme from './theme';
+
+export default combineReducers({font, theme});
